ed292bfd676e4d422f781dd369b1724f280c860c
Sindre Moldeklev <sndrem@gmail.com>
2018-11-11 21:35:06 +0100

    La til riktig databasenavn

--- config/database.js
+++ config/database.js
@@ -1,20 +1,20 @@
 const Influx = require("influx");
 
 const influx = new Influx.InfluxDB({
-    host: process.env.INFLUX_HOST,
-    database: process.env.INFLUX_DB || "homeassistant",
-    schema: [
-        {
-            measurement: "uptime",
-            fields: {
-                status_name: Influx.FieldType.STRING,
-                site_name: Influx.FieldType.STRING,
-                status: Influx.FieldType.BOOLEAN,
-                status_number: Influx.FieldType.INTEGER
-            },
-            tags: ["uptime_bot"]
-        }
-    ]
+  host: process.env.INFLUX_HOST,
+  database: process.env.INFLUX_DB || "home_assistant",
+  schema: [
+    {
+      measurement: "uptime",
+      fields: {
+        status_name: Influx.FieldType.STRING,
+        site_name: Influx.FieldType.STRING,
+        status: Influx.FieldType.BOOLEAN,
+        status_number: Influx.FieldType.INTEGER
+      },
+      tags: ["uptime_bot"]
+    }
+  ]
 });
 
 module.exports = influx;
